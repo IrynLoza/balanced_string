72cdbe4fc3717b435170bbe66eeeebf9ea890351
Iryna Loza <irynloza@gmail.com>
2021-02-11 17:27:29 -0800

    Complete balanced string

--- balanced_string.py
+++ balanced_string.py
@@ -9,8 +9,25 @@
 -1
 """
 
+def check_register(s):
+    for i in range(len(s)):
+        if s[i].islower() and s[i].upper() not in s:
+            return False
+        if s[i].isupper() and s[i].lower() not in s:
+            return False
+    return True
+
 def balanced_string(s):
-    pass
+    min_num = len(s)
+    isMin = False
+    for i in range(len(s)+1):
+        for j in range(i+1, len(s)+1):
+            if check_register(s[i:j]) and min_num >= j-i:
+                min_num = j-i
+                isMin = True
+    if isMin:
+        return min_num
+    return -1
 
 
 
@@ -18,4 +35,4 @@ def balanced_string(s):
 if __name__ == '__main__':
     import doctest
     if doctest.testmod().failed == 0:
-        print('\n✨ YOU ARE STAR! ALL TESTS PASSED!\n')
+        print('\n✨ ROCK STAR! ALL TESTS PASSED!\n')
