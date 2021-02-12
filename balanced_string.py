80fa0d33496d0e06764163af81c634eb38170bd9
Iryna Loza <irynloza@gmail.com>
2021-02-11 17:25:48 -0800

    Initial commit

---
+++ balanced_string.py
@@ -0,0 +1,21 @@
+""":
+>>> balanced_string('azABaabza')
+5
+>>> balanced_string('TocoCat')
+-1
+>>> balanced_string('AcZCbaBz')
+8
+>>> balanced_string('abcdefghijklmnopqrstuvwxyz')
+-1
+"""
+
+def balanced_string(s):
+    pass
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n✨ YOU ARE STAR! ALL TESTS PASSED!\n')
